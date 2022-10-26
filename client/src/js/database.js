47d15b88c09a15611e090e5fa46b94a1202a13cc
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-25 20:21:29 -0500

    add const variable for database connection, new transaction and object store

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // create connection to the database and version
+  const jateDB = await openDB("jate", 1);
+  // create a new transaction and which database is used and the data privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // open up the object store
+  const store = tx.objectStore("jate");
+  
+  console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
