dc849586f2800302261a3c4c81049d6165cbff82
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-25 20:36:41 -0500

    add const variables for connection to db and create new transaction in getDB, fix putDB const tx variable

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   // create connection to the database and version
   const jateDB = await openDB("jate", 1);
   // create a new transaction and which database is used and the data privileges
-  const tx = jateDB.transaction("jate", "readonly");
+  const tx = jateDB.transaction("jate", "readwrite");
   // open up the object store
   const store = tx.objectStore("jate");
   // use put method for the store and pass in content
@@ -29,6 +29,12 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  // create connection to the database and version
+  const jateDB = await openDB("jate", 1);
+  // create a new transaction and which database is used and the data privileges
+  const tx = jateDB.transaction("jate", "readonly");
+};
 
 initdb();
