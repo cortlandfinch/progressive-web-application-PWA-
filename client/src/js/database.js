ab348adf406cc63e439f55615be654c11b6e760b
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-25 20:32:17 -0500

    add method to add to the database, add store.put and pass content and call request

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,14 +14,18 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.error('putDb not implemented');
   // create connection to the database and version
   const jateDB = await openDB("jate", 1);
   // create a new transaction and which database is used and the data privileges
   const tx = jateDB.transaction("jate", "readonly");
   // open up the object store
   const store = tx.objectStore("jate");
-  
-  console.error('putDb not implemented');
+  // use put method for the store and pass in content
+  const request = store.put({ jate: content });
+  // get the confirmation for the request
+  const result = await request;
+  console.log('Your data was saved to your database!', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
